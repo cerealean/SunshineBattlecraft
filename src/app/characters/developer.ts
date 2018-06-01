57fccde592bf9f1b9918dea0cdc1da45fdfb7291
Michael Crawford <Michael.Crawford@vu.com>
2018-06-01 15:07:46 -0500

    Addec constructor

--- src/app/characters/developer.ts
+++ src/app/characters/developer.ts
@@ -1,8 +1,16 @@
-import { Character } from "./character";
+import { Character } from './character';
 
 export class Developer extends Character {
-    public name = "Developer";
+    public name = 'Developer';
     public health = 10;
     public defence = 1;
     public speed = 2;
+
+    /**
+     *
+     */
+    constructor(name: string) {
+        super();
+        this.name = name;
+    }
 }
