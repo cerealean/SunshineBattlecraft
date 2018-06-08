dd5eae8f32835b0adc4e9c9766af927cd67dc983
Michael Crawford <Michael.Crawford@vu.com>
2018-06-08 13:51:37 -0500

    Included bulma and made base navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiceRollerrComponent } from './dice-rollerr/dice-rollerr.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DiceRollerrComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
