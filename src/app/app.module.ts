5bf4e13aeeecea4727dbc05bf4f117135d2de45b
Michael Crawford <Michael.Crawford@vu.com>
2018-06-08 13:57:57 -0500

    Added structures and units components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StructuresComponent } from './structures/structures.component';
+import { UnitsComponent } from './units/units.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StructuresComponent,
+    UnitsComponent
   ],
   imports: [
     BrowserModule,
